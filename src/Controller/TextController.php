06fd1ef7a8bfcb95ea33ab3fff910be35d3e1651
547358880 <547358880@qq.com>
2016-06-16 11:01:37 +0800

    第一次提交

---
+++ src/Controller/TextController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xujing
+ * Date: 2016/6/16
+ * Time: 10:36
+ * Description
+ */
+namespace App\Controller;
+
+class TextController
+{
+    public function __construct()
+    {
+        echo 'dasdasd';
+    }
+}
